2673620c1b0decabac9ac521493804ebe7cc6e40
Greg Pfeil <greg@technomadic.org>
2024-11-11 15:33:11 -0700

    Update interpreter test output
    This also makes a couple minor changes re: running the script:
    
    - removes the “belt and suspenders” `echo`ing that resulted in things being printed in triplicate
    - added `gettext` to the Nix environment,  so `envsubst` is available
    - changed the #!, to not get stuck with Bash 3.2 on macOS.

--- unison-src/builtin-tests/interpreter-tests.sh
+++ unison-src/builtin-tests/interpreter-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -ex
 
 if [ -z "$1" ]; then
@@ -8,7 +8,6 @@ else
 fi
 
 runtime_tests_version="@unison/runtime-tests/releases/0.0.1"
-echo $runtime_tests_version
 
 codebase=${XDG_CACHE_HOME:-"$HOME/.cache"}/unisonlanguage/runtime-tests.unison
 
@@ -16,5 +15,4 @@ runtime_tests_version="$runtime_tests_version" \
     envsubst '$runtime_tests_version' \
     < unison-src/builtin-tests/interpreter-tests.tpl.md \
     > unison-src/builtin-tests/interpreter-tests.md
-echo "$ucm" transcript.fork -C $codebase -S $codebase unison-src/builtin-tests/interpreter-tests.md
 time "$ucm" transcript.fork -C $codebase -S $codebase unison-src/builtin-tests/interpreter-tests.md
