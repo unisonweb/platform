c5d9f883dc4444113c3fbcdd0eb1df86446a9cec
Brandon Barker <brandon.barker@gmail.com>
2024-12-21 08:57:59 -0500

    remove comment

--- unison-syntax/test/Main.hs
+++ unison-syntax/test/Main.hs
@@ -7,9 +7,6 @@ import System.IO.CodePage (withCP65001)
 import Unison.Test.Doc qualified as Doc
 import Unison.Test.Unison qualified as Unison
 
--- main :: IO ()
--- main = withCP65001 . run $ tests [Unison.test, Doc.test]
-
 test :: Test ()
 test =
   tests
